fa20b0c0c6a9c053cce001f0fa86fcb827fb2af6
yfl123456 <1143932362@qq.com>
2020-04-21 09:56:53 +0800

    创建框架

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="page-home">
-    <h1>首页</h1>
+    <header class="index-header">
+      <a href="">
+        <div class="header-user">
+          <div class="user-btn"></div>
+        </div>
+      </a>
+      <div class="header-logo"></div>
+      <a href="">
+        <div class="header-search"></div>
+      </a>
+    </header>
+    <div class="banner" style="background:pink;height: 150px;"></div>
   </div>
 </template>
 
@@ -10,4 +21,36 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.page-home {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
+.index-header {
+  padding: 0 20px;
+  box-sizing: border-box;
+  display: flex;
+  height: 44px;
+  justify-content: space-between;
+  align-items: center;
+  .user-btn{
+    width: 25px;
+    height: 25px;
+    background: url("../../assets/icon/user-btn.png") no-repeat center center;
+    background-size: 100%;
+  }
+  .header-logo{
+    width: 92px;
+    height: 28px;
+    background: url("../../assets/icon/logo.png") no-repeat center center;
+    background-size: 100%;
+  }
+  .header-search{
+    height: 25px;
+    width: 25px;
+    background: url("../../assets/icon/scoat.png") no-repeat center center;
+    background-size: 100%;
+  }
+}
+</style>
